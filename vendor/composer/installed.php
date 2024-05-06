d2a06755bf6964c68d6cd0008765c7649c5d8ef4
pierrefrancois <pf@romanliturgy.org>
2024-05-06 16:45:53 +0200

    add vendor directory

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'smarty/smarty' => 
+    array (
+      'pretty_version' => 'v4.4.1',
+      'version' => '4.4.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f4152e9b814ae2369b6e4935c05e1e0c3654318d',
+    ),
+  ),
+);
